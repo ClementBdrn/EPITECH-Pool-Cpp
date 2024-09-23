57cea1ec0ed0cfaa83001699c8e1eb74a6c7ca16
Clément BADRONE <clement.badrone@epitech.eu>
2024-09-23 17:12:03 +0200

    Add files via upload

--- Day 2/C++_Pool_Day2.cpp
+++ Day 2/C++_Pool_Day2.cpp
@@ -51,6 +51,52 @@ namespace Federation
 {
     namespace Starfleet
     {
+        class Captain
+        {
+        private:
+            // Setup variable
+            std::string _name;
+            int _age;
+
+        public:
+            // Constructor
+            Captain(std::string name) : _name(name)
+            {
+            }
+
+            // Return the name of the captain
+            std::string getName()
+            {
+                return _name;
+            }
+
+            // Return the age of the captain
+            int getAge()
+            {
+                return _age;
+            }
+
+            // Set the age of the captain
+            void setAge(int age)
+            {
+                _age = age;
+            }
+        };
+
+        class Ensign
+        {
+        private:
+            // Setup variable
+            std::string _name;
+
+        public:
+            // Constructor
+            Ensign(std::string name) : _name(name)
+            {
+                std::cout << "Ensign " << _name << ", awaiting for orders." << std::endl;
+            }
+        };
+
         class Ship
         {
         private:
@@ -60,6 +106,7 @@ namespace Federation
             std::string _name;
             short _maxWrap;
             WarpSystem::Core *_coreReactor = nullptr;
+            Captain *_captain = nullptr;
 
         public:
             // Constructor
@@ -87,6 +134,13 @@ namespace Federation
                 {
                     std::cout << "USS " << _name << " : No core found!" << std::endl;
                 }
+            }
+
+            // Promote a new captain of the ship
+            void promote(Captain *captain)
+            {
+                _captain = captain;
+                std::cout << _captain->getName() << " : I'm glad to be the captain of the USS " << _name << "." << std::endl;
             }
         };
     }
@@ -131,6 +185,45 @@ namespace Federation
     };
 }
 
+namespace Borg
+{
+    class Ship
+    {
+    private:
+        // Setup variables
+        int _side = 300;
+        short _maxWrap = 9;
+        WarpSystem::Core *_coreReactor = nullptr;
+
+    public:
+        // Constructor
+        Ship()
+        {
+            std::cout << "We are the Borgs . Lower your shields and surrender yourselves unconditionally. \nYour biological characteristics and technologies will be assimilated. \nResistance is futile." << std::endl;
+        }
+
+        // Setup the value of the core
+        void setupCore(WarpSystem::Core *core)
+        {
+            _coreReactor = core;
+        }
+
+        // Print the state of _stability
+        void checkCore() const
+        {
+            if (_coreReactor != nullptr)
+            {
+                bool stability = _coreReactor->checkReactor()->isStable();
+                std::cout << "Everything is in order." << std::endl;
+            }
+            else
+            {
+                std::cout << "Critical failure iminent." << std::endl;
+            }
+        }
+    };
+}
+
 int main()
 {
     // Exercice_0
@@ -148,5 +241,21 @@ int main()
     QR2.setStability(false);
     UssJanissary.checkCore();
     Independent.checkCore();
+
+    // Exercice_1
+    Federation ::Starfleet ::Ship UssJanissary(289, 132, " Janissary ", 6);
+    Federation ::Starfleet ::Captain James(" James T. Kirk ");
+    Federation ::Starfleet ::Ensign Ensign(" Pavel Chekov ");
+    WarpSystem ::QuantumReactor QR;
+    WarpSystem ::QuantumReactor QR2;
+    WarpSystem ::Core core(&QR);
+    WarpSystem ::Core core2(&QR2);
+    UssJanissary.setupCore(&core);
+    UssJanissary.checkCore();
+    UssJanissary.promote(&James);
+    Borg ::Ship Cube;
+    Cube.setupCore(&core2);
+    Cube.checkCore();
+
     return 0;
 }
